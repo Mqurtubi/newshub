48a8ed48134e7481c34ec4ece3fe3a178d8bb708
Muhammad Qurtubi <qurtubi1411@gmail.com>
2025-09-06 19:30:31 +0700

    update

---
+++ src/api/news.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const API_KEY= import.meta.env.VITE_OWM_KEY;
+
+const getNews= async ()=>{
+    try {
+        const endPoint="https://newsapi.org/v2/everything";
+        const res=await axios.get(endPoint,{
+            params:{
+                q:'bitcoin',
+                apikey:API_KEY,
+            }
+        })
+        return res.data
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const getNewsTopHeadlines= async ()=>{
+    try{
+        const endPoint="https://newsapi.org/v2/top-headlines"
+        const res=await axios.get(endPoint,{
+            params:{
+                country:"us",
+                apikey:API_KEY
+            }
+        })
+        return res.data
+    }catch(error){
+        console.log(error.message)
+    }
+}
+
+export{getNews, getNewsTopHeadlines}
