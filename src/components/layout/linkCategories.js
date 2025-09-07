9d6d72428816c53ed43753f6138290cf71c7af2e
Muhammad Qurtubi <qurtubi1411@gmail.com>
2025-09-07 21:01:59 +0700

    add: footer item

---
+++ src/components/layout/linkCategories.js
@@ -0,0 +1,23 @@
+const link = [
+        {
+            href:"/",
+            label:"Home"
+        },
+        {
+            href:"/politics",
+            label:"Politics"
+        },
+        {
+            href:"/technology",
+            label:"Technology"
+        },
+        {
+            href:"/sport",
+            label:"Sport"
+        },
+        {
+            href:"/business",
+            label:"Business"
+        }
+]
+export default link
