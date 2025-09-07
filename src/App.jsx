cd7e7e82fc0e1460bc2c314aefcec7222ed006fd
Muhammad Qurtubi <qurtubi1411@gmail.com>
2025-09-07 19:38:08 +0700

    add: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useEffect } from "react"
 import { getNews, getNewsTopHeadlines } from "./api/news"
+import { Route, Routes} from "react-router-dom"
+import NavBar from "./components/layout/NavBar"
 function App() {
   useEffect(()=>{
     getNewsTopHeadlines().then(res => console.log(res))
   },[])
   return (
-    <>
-      
-    </>
+    <div>
+      <NavBar/>
+      <Routes>
+        
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/layout/NavBar.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { FaNewspaper } from "react-icons/fa6";
+import { IoMenu, IoSearch, IoClose } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
+export default function NavBar(){
+    const [open,setOpen]=useState(false)
+
+    const handleClick=()=>setOpen(!open)
+    const link = [
+        {
+            href:"/",
+            label:"Home"
+        },
+        {
+            href:"/politics",
+            label:"Politics"
+        },
+        {
+            href:"/technology",
+            label:"Technology"
+        },
+        {
+            href:"/sport",
+            label:"Sport"
+        },
+        {
+            href:"/business",
+            label:"Business"
+        }
+    ]
+    return(
+        <div>
+        <div className="flex justify-between pt-2 pb-4 shadow-md px-5">
+            <div className="flex items-center space-x-3 lg:space-x-7">
+            <div className="flex font-semibold items-center space-x-1 text-xl">
+                <FaNewspaper className="text-blue-800"/>
+                <p>News<span className="text-blue-800">Hub</span></p>
+            </div>
+            <div className="hidden md:flex  space-x-5">
+                {
+                    link.map((item,index)=>(
+                            <NavLink to={item.href} className={({isActive})=> isActive ? "text-blue-800 font-semibold":"text-slate-600"} key={index}>{item.label}</NavLink>
+
+                    ))
+                }
+            </div>
+        </div>
+            <div className="text-3xl hover:cursor-pointer flex space-x-3">
+                <div className="relative items-center hidden sm:flex text-lg ">
+                    <input type="text" className="border border-slate-400 rounded pl-10 focus:outline-none w-full" placeholder="Search news..."/>
+                    <IoSearch className="absolute left-3 text-slate-400"/>
+                </div>
+                <div className="md:hidden">
+                {open ? (
+                    <IoClose onClick={handleClick}/>
+                ):(
+                    <IoMenu  onClick={handleClick}/>
+                )}
+                </div>
+            </div>
+        </div>
+        <div className={`overflow-hidden transition-all duration-300 ease-in-out ${open ? "max-h-96 opacity-100":"max-h-0 opacity-0"}`}>
+        <div className="flex flex-col space-y-3 px-4 py-4 bg-blue-500 text-white md:hidden">
+            <div className="relative flex items-center sm:hidden">
+                <input type="text" className="border rounded pl-10 focus:outline-none w-full py-1" placeholder="Search news..."/>
+                <IoSearch className="absolute left-3"/>
+            </div>
+            {
+                link.map((item,index)=>(
+                        <NavLink to={item.href} className={({isActive})=> isActive ? "text-slate-800 font-semibold":"text-white"} key={index}>{item.label}</NavLink>
+                ))
+            }
+        </div>
+        </div>
+        </div>
+    )
+}
